65430add99c1af2f41879405374dc4f29f8ad7d8
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-03 18:10:51 +0600

    Send data to database and store in mongodb

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -5,11 +5,39 @@ import { UserContext } from '../../App';
 const Book = () => {
     const [loggedInUser, setLoggedInUser]=useContext(UserContext);
     const {bedType} = useParams();
+    const handleIT=()=>{
+     const name=   document.getElementById("name").value;
+      const age =  document.getElementById("age").value;
+      const info ={name,age}
+      console.log(info);
+        fetch("http://localhost:3002/allInfo",{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+              body: JSON.stringify({name,age}),
+              
+
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+        });
+
+
+    }
     return (
         <div style={{textAlign: 'center'}}>
             <p>{loggedInUser.name}</p>
             <h1>Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
+
+            <input type="text" name="name" placeholder="name" id="name" />
+            <br /><br />
+            <input type="text" name="age" placeholder="age" id="age" />
+            <br /> <br />
+            <button onClick={handleIT}>Sumbit to mongodb</button>
+
         </div>
     );
 };
